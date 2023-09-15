8f89f33784264dfcddad573628b4d2aaa7a11e3b
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-15 14:21:31 +0200

    [UPDATE]: file script

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,23 @@
 1. Creo una variabile dove chiedo all'utente di inserire una parola;
 
 2. Creo una funzione dove controllare se la parola è palindroma o no;
+   a. Controllo se il valore passato sia effettivamente una stringa;
+   b. 
 
 3. Stampo il risultato corrispondente
 */
 
 // 1. 
 const requestWord = prompt('Inserisci una parola');
-console.log(requestWord);
+
+
+palindroma(requestWord);
+
+// 2. 
+function palindroma (string) {
+    if(isNaN(string)) {
+console.log('CORRETTO: è una stringa');
+    }else{
+        console.log('ERRORE: Devi inserire una stringa');
+    };
+};
