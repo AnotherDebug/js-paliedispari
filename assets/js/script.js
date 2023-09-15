4bb484ed7293321132829c06b3a56ba3e4ac4354
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-15 14:09:49 +0200

    [UPDATE]: file script

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* 
+## Palidroma ##
+
+- Chiedere all’utente di inserire una parola;
+- Creare una funzione per capire se la parola inserita è palindroma.
+
+################################################################################################################
+
+1. Creo una variabile dove chiedo all'utente di inserire una parola;
+
+2. Creo una funzione dove controllare se la parola è palindroma o no;
+
+3. Stampo il risultato corrispondente
+*/
+
+// 1. 
+const requestWord = prompt('Inserisci una parola');
+console.log(requestWord);
